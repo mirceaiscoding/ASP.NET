5249f22362c7192f4656c29b2c3533b7f5b7ecfa
Bina Mircea <bina.mircea2001@gmail.com>
2021-11-11 22:15:59 +0200

    #1 Added Authentication Entities

---
+++ ReservationsAPI/ReservationsAPI/DAL/Entities/Role.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace ReservationsAPI.DAL.Entities
+{
+    public class Role : IdentityRole<int>
+    {
+        public virtual ICollection<UserRole> UserRoles { get; set; }
+    }
+}
---
+++ ReservationsAPI/ReservationsAPI/DAL/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace ReservationsAPI.DAL.Entities
+{
+    public class User : IdentityUser<int>
+    {
+        public virtual ICollection<UserRole> UserRoles { get; set; }
+    }
+}
