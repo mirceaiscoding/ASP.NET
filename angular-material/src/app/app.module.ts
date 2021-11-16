8c9dd0686e0f6b38e30210c04623c015c078c6cf
Bina Mircea <bina.mircea2001@gmail.com>
2021-11-17 00:56:41 +0200

    #2 Generated navbar empty component

--- angular-material/src/app/app.module.ts
+++ angular-material/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
