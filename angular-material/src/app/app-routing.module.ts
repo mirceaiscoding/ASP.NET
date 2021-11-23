bea108bd563872384717ea02701e25a1c3321023
Bina Mircea <bina.mircea2001@gmail.com>
2021-11-23 19:30:21 +0200

    #2 Updated app component to display router outlet

--- angular-material/src/app/app-routing.module.ts
+++ angular-material/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 
-
+const routes: Routes = [
+  { path: 'home', component: HomepageComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
