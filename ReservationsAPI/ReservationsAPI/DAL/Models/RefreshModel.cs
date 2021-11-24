198b6fd215fdf7a42295b09b83e02a5cdb9cb7be
Bina Mircea <bina.mircea2001@gmail.com>
2021-11-24 18:21:26 +0200

    #1 Added refresh token and database migration

---
+++ ReservationsAPI/ReservationsAPI/DAL/Models/RefreshModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ReservationsAPI.DAL.Models
+{
+    public class RefreshModel
+    {
+        public string AccessToken { get; set; }
+
+        public string RefreshToken { get; set; }
+    }
+}
