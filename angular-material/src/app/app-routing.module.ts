a3c058105660de1b2874995ab029c3543af90f57
Bina Mircea <bina.mircea2001@gmail.com>
2021-12-05 00:26:52 +0200

    Updated routing

--- angular-material/src/app/app-routing.module.ts
+++ angular-material/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DoctorsPresentationComponent } from './pages/doctors-presentation/doctors-presentation.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomepageComponent },
   { path: 'doctors', component: DoctorsPresentationComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
--- angular-material/src/app/pages/login/login.component.ts
+++ angular-material/src/app/pages/login/login.component.ts
@@ -12,15 +12,9 @@ import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
 export class LoginComponent implements OnInit {
 
   constructor(private authService: AuthService,
-    private router: Router,
-    private formBuilder: FormBuilder) { }
+    private router: Router) { }
 
   emailRegx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  // loginForm: FormGroup = this.formBuilder.group({
-  //   email: [null, [Validators.required, Validators.pattern(this.emailRegx)]],
-  //   password: [null, Validators.required]
-  // });
 
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.pattern(this.emailRegx)]),
@@ -29,6 +23,11 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void { }
 
+  goToRegisterPage() {
+    this.router.navigate(['/register'])
+  }
+
+  // Submits login form
   submit() {
     if (!this.loginForm.valid) {
       console.log("Invalid form!");
@@ -37,7 +36,7 @@ export class LoginComponent implements OnInit {
     var loginData: AuthModel = this.loginForm.value;
     console.log(loginData);
 
-    // APELAM SERVICIU LOGIN
+    // Call auth service
     this.authService.login(loginData).subscribe((response: any) => {
       console.log(response);
       if (response && response['accessToken'] && response['refreshToken']) {
