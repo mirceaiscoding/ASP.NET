48b21c825f274a2bda44a9941e3caff302951530
Bina Mircea <bina.mircea2001@gmail.com>
2022-01-12 15:29:18 +0200

    Updated routing module

--- angular-material/src/app/app-routing.module.ts
+++ angular-material/src/app/app-routing.module.ts
@@ -13,18 +13,47 @@ import { ProceduresComponent } from './pages/procedures/procedures.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomepageComponent },
-  { path: 'doctors', component: DoctorsPresentationComponent },
-  { path: 'appointments', component: AppointmentsComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'services', component: ProceduresComponent },
+  { 
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full' 
+  },
+  { 
+    path: 'home', 
+    component: HomepageComponent 
+  },
+  { 
+    path: 'doctors', 
+    component: DoctorsPresentationComponent 
+  },
+  { 
+    path: 'appointments',
+    component: AppointmentsComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: {roles: ["Admin"]}
+
+  },
+  { 
+    path: 'dashboard', 
+    component: DashboardComponent, 
+    canActivate: [AuthGuard],
+  },
+  { 
+    path: 'login', 
+    component: LoginComponent 
+  },
+  { 
+    path: 'register', 
+    component: RegisterComponent 
+  },
+  { 
+    path: 'services', 
+    component: ProceduresComponent },
   {
     path: 'profile/:id',
     component: PacientProfileComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard, RoleGuard],
+    data: {roles: ["Pacient"]}  
   },
   { 
     path: 'make-appointment/:id', 
